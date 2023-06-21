3dbff56997b9d05964cbd74ce6769f4f91f2b765
Ankit Kumar Thakur <animsanddccandgm@gmail.com>
2023-06-21 13:14:14 +0530

    Some Functionality Added

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -37,9 +37,9 @@ class RegistrationController extends Controller
         return view("user-auth.login");
     }
 
-    public function processLogin(Request  $request){
-
-         $rule = array(
+    public function processLogin(Request  $request)
+    {
+        $rule = array(
             'user_id' => 'required',
             'password' => 'required'
         );
@@ -47,41 +47,43 @@ class RegistrationController extends Controller
             'user_id.required' => 'User ID is required',
             'password.required' => 'Password is required',
         );
-
-
-          $validation = Validator::make($request->all(), $rule, $messages);
-        if ($validation->fails()) {
+        $validation = Validator::make($request->all(), $rule, $messages);
+        if ($validation->fails()){
             return redirect()->back()->withInput()->withErrors($validation);
         }
-
-            $cred = [
-                'user_id' => $request->user_id,
-                'password' => $request->password,
-            ];
-            // $hashedPassword = Hash::make($cred['password']);
-            $user = Registration::where("userid", $cred['user_id'])->where("password", $cred['password'])->first();
-        
-            if(!empty($user)){
+        $cred = [
+            'user_id' => $request->user_id,
+            'password' => $request->password,
+        ];
+        $user = Registration::where("userid", $cred['user_id'])->first();
+        if(!empty($user))
+        {
+            if( ! Hash::check($cred['password'] , $user->password ) )
+            {
+                $message = 'password is incorrect';
+                return redirect()->back()->with('error', $message)->withInput();
+            }else
+            {
                 if($user->is_verify == 0){
-            $message = 'Please verfiy Mail Id!';
-            return redirect()->back()->with('error', $message)->withInput();
-         }
-            $data=[
-                'first_name' => $user->first_name,
-                'last_name' =>$user->last_name,
-                'userid' =>$user->userid,
-                'email' =>$user->email,
-            ];
+                    $message = 'Please verfiy Mail Id!';
+                    return redirect()->back()->with('error', $message)->withInput();
+                }
+                $data=[
+                    'first_name' => $user->first_name,
+                    'last_name' =>$user->last_name,
+                    'userid' =>$user->userid,
+                    'email' =>$user->email,
+                ];
 
-            Session::put('user', $data);
-            return Redirect()->to('/dashboard');
+                Session::put('user', $data);
+                return Redirect()->to('/dashboard');
                 $message = 'Successfully login!';
                 return redirect()->back()->with('success', $message)->withInput();
-            }else{
-            $message = 'username or password is incorrect';
-                return redirect()->back()->with('error', $message)->withInput();
             }
-        
+        }else{
+            $message = 'username or password is incorrect';
+            return redirect()->back()->with('error', $message)->withInput();
+        }
     }
 
     //create logout function 
